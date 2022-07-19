5ea7e69198cc78cca35ba2670775fc72abc332f9
Mark Vogelgesang <mark.vogelgesang@gsa.gov>
2022-07-19 09:23:57 -0400

    Added documentation

--- typescript/cli-resume-activity/src/hooks/state_manager/create.ts
+++ typescript/cli-resume-activity/src/hooks/state_manager/create.ts
@@ -1,8 +1,22 @@
 import {Hook} from '@oclif/core'
-import {writeFileSync} from 'node:fs'
+import {existsSync, mkdirSync, writeFileSync} from 'node:fs'
 
+/**
+ * Hook implementation to create a locally cached file which will allow for resumption of activities. Writes a json file to the cacheDir.
+ * @param options object containing 'command' and 'data' keys
+ * @returns Promise<void>
+ */
 const hook: Hook<'state_manager:create'> = async function (options): Promise<void> {
-  process.stdout.write('Caching the list\n')
+  if (!options.command || !options.nameList) {
+    this.log('Missing parameters to create cached file, exiting...', 'info')
+    return
+  }
+
+  this.log('Caching the list\n')
+  if (!existsSync(this.config.cacheDir)) {
+    mkdirSync(this.config.cacheDir)
+  }
+
   writeFileSync(`${this.config.cacheDir}/nameList.json`, JSON.stringify(options.nameList))
 }
 
--- typescript/cli-resume-activity/src/hooks/state_manager/update.ts
+++ typescript/cli-resume-activity/src/hooks/state_manager/update.ts
@@ -2,8 +2,12 @@ import {Hook} from '@oclif/core'
 import {writeFileSync} from 'node:fs'
 import {retrieveFile} from './helper'
 
+/**
+ * Provided the name of a json file, opens it and removes the first element in the array.
+ * @returns Promise<void>
+ */
 const hook: Hook<'state_manager:update'> = async function (): Promise<void> {
-  process.stdout.write('Removing name from list\n')
+  this.log('Removing name from list\n')
   const fileName = `${this.config.cacheDir}/nameList.json`
   const data = await retrieveFile(fileName)
   data.shift()
