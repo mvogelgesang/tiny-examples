b33a4c1e78c5c634a6aeb6b36f5ed3bc953e6f7b
Mark Vogelgesang <mark.vogelgesang@gsa.gov>
2022-07-19 09:25:58 -0400

    Added documentation (#8)

--- typescript/cli-resume-activity/src/hooks/state_manager/retrieve.ts
+++ typescript/cli-resume-activity/src/hooks/state_manager/retrieve.ts
@@ -1,6 +1,10 @@
 import {Hook} from '@oclif/core'
 import {retrieveFile} from './helper'
 
+/**
+ * Given a filename, retrieves the json file from the local cache directory
+ * @returns array
+ */
 const hook: Hook<'state_manager:retrieve'> = async function (): Promise<string[]> {
   const fileName = `${this.config.cacheDir}/nameList.json`
   return retrieveFile(fileName)
