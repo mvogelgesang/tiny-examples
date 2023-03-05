67ac58214f101d9a117f8bacc686ecf3a82ad828
Mark Vogelgesang <mark.vogelgesang@gsa.gov>
2023-03-04 21:30:16 -0500

    initial working example and comments

---
+++ typescript/type-safe-inputs/src/index.ts
@@ -0,0 +1,18 @@
+/* https://dev.to/hansott/how-to-check-if-string-is-member-of-union-type-1j4m */
+/* quality explanation, https://stackoverflow.com/questions/66993264/what-does-the-as-const-mean-in-typescript-and-what-is-its-use-case */
+const VOWELS = ["a", "e", "i", "o", "u"] as const;
+type VowelTuple = typeof VOWELS;
+// uses indexed access types to basically convert a readonly array to a union type
+// https://www.typescriptlang.org/docs/handbook/2/indexed-access-types.html#handbook-content
+type Vowel = VowelTuple[number];
+let argv = require("minimist")(process.argv.slice(2));
+
+function isVowel(value: string): value is Vowel {
+  return VOWELS.includes(value as Vowel);
+}
+console.log(argv);
+
+argv._.forEach((element: string) => {
+  console.log(element);
+  console.log(isVowel(element));
+});
