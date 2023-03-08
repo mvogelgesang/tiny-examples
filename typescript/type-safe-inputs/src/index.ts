c4d97cd1daff1166342af6c5b2137c858545b954
Mark Vogelgesang <mark.vogelgesang@gsa.gov>
2023-03-08 08:06:29 -0500

    added additional reference links to comments

--- typescript/type-safe-inputs/src/index.ts
+++ typescript/type-safe-inputs/src/index.ts
@@ -1,5 +1,7 @@
 /* https://dev.to/hansott/how-to-check-if-string-is-member-of-union-type-1j4m */
-/* quality explanation, https://stackoverflow.com/questions/66993264/what-does-the-as-const-mean-in-typescript-and-what-is-its-use-case */
+/* quality explanation, https://stackoverflow.com/questions/66993264/what-does-the-as-const-mean-in-typescript-and-what-is-its-use-case 
+https://stackoverflow.com/questions/52085454/typescript-define-a-union-type-from-an-array-of-strings
+*/
 const VOWELS = ["a", "e", "i", "o", "u"] as const;
 type VowelTuple = typeof VOWELS;
 // uses indexed access types to basically convert a readonly array to a union type
